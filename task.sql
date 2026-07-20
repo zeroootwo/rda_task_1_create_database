0ac533d42fd276b05e14a9116530ec89a52c447f
Anastasiia Velikaya <a.v.zaychenko@gmail.com>
2026-07-20 22:36:04 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,33 @@
-# Write your SQL code for the database creation here. Good luck! 
+CREATE DATABASE ShopDB;
+USE ShopDB;
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT,
+    `Name` VARCHAR(100),
+    `Description` VARCHAR(100),
+    Price INT,
+    WarehouseAmount INT,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE Customers(
+    ID INT AUTO_INCREMENT,
+    FirstName VARCHAR(100),
+    LastName VARCHAR(100),
+    Email VARCHAR(100),
+    Address VARCHAR(100),
+    PRIMARY KEY (ID)
+);
+CREATE TABLE Orders(
+    ID INT AUTO_INCREMENT,
+    CustomerID INT,
+    `Date` DATE,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
+);
+CREATE TABLE OrderItems(
+    ID INT AUTO_INCREMENT,
+    OrderID INT,
+    ProductID INT,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+);
--- test.sql
+++ test.sql
@@ -1,18 +1,13 @@
-# check if database exists 
-USE ShopDB; 
+USE ShopDB;
 
-# check if table "Products" exists, all required columnts are in place, and have correct types 
 INSERT INTO Products (Name, Description, Price, WarehouseAmount)
 	VALUES ('Product', 'Product Desctiption', 5, 42);
 
-# check if table "Customers" exists, all required columns are in place, and have correct types
 INSERT INTO Customers (FirstName, LastName, Email, Address)
 	VALUES ('John', 'Dou', 'j@dou.ua', 'far, far away');
 
-# check if table "Orders" exists, all required columns are in place, and have correct types
 INSERT INTO Orders (CustomerID, Date)
 	VALUES (1, '2023-01-01');
 
-# check if table "OrderItems" exists, all required columns are in place, and have correct types
 INSERT INTO OrderItems (OrderID, ProductID)
 	VALUES (1, 1);
